7115a11b5e6ca22e227e4bfad18d46e50ab0f423
onelazycat <onelazycat@foxmail.com>
2025-08-07 18:32:25 +0800

    gee-3

--- gee/router.go
+++ gee/router.go
@@ -1,23 +1,61 @@
 package gee
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 // router 定义一个路由,存在所有匹配模式和处理方法
 type router struct {
+	roots    map[string]*node
 	handlers map[string]HandlerFunc
 }
 
 // newRouter 创建时路由实例
 func newRouter() *router {
 	return &router{
+		roots:    make(map[string]*node),
 		handlers: make(map[string]HandlerFunc),
 	}
 }
 
+// parsePattern 解析路由模式，拆分api路径地址；
+// 比如 “/api/user/:id” => [api,user,:id]
+func parsePattern(pattern string) []string {
+	segs := strings.Split(pattern, "/")
+	parts := make([]string, 0)
+	for _, seg := range segs {
+		if seg != "" { // 路由模式经过strings.Split后第一个通常是空字符
+			parts = append(parts, seg)
+			if seg[0] == '*' { // 比如：*filepath
+				break
+			}
+		}
+	}
+
+	return parts
+}
+
 // addRoute 添加（注册）路由
 func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	parts := parsePattern(pattern)
 	key := method + "-" + pattern
+	// 如果还没有该http method方法节点，则创建
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+
+	// 在指定method节点下添加
+	r.roots[method].insert(pattern, parts, 0)
 	r.handlers[key] = handler
+	/* roots 以http的method方法为第一个节点，
+		  roots
+		/   |   \
+	GET  POST PUT  ...
+	/|\  /|\  /|\
+	.............
+	*/
 }
 
 // handle 分发路由处理
--- gee/trie.go
+++ gee/trie.go
@@ -14,5 +14,52 @@ func (n *node) String() string {
 	return fmt.Sprintf("node{pattern=%s, part=%s, isWild=%t}", n.pattern, n.part, n.isWild)
 }
 
+func (n *node) insert(pattern string, parts []string, height int) {
+	fmt.Printf("insert pattern=%q, parts=%q height=%q", pattern, parts, height)
+	// 退出递归，添加路由height是从0开始；
+	// 比如：pattern="/api/user/:id", parts=[api,user,:id],
+	// 当len(parts) == height-1时，parts元素都已经添加完了
+	// 因此len(parts) == height，已经是没有元素可以insert了，将pattern赋值给最后一个node,退出
+	if len(parts) == height {
+		n.pattern = pattern
+	}
 
-func ()
+	part := parts[height]       // 获取要添加的节点
+	child := n.matchChild(part) // 查找当前节点是否匹配存在，不存在则添加，存在则插入下一个节点，
+
+	if child == nil {
+		child = &node{
+			part:   part,
+			isWild: part[0] == ':' || part[0] == '*', // 是否采用模糊匹配或者说精准匹配
+		}
+	}
+
+}
+
+func (n *node) travel(list *([]*node)) {
+	if n.pattern != "" {
+		*list = append(*list, n)
+	}
+	for _, child := range n.children {
+		child.travel(list)
+	}
+}
+
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
